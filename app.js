40a0ee87f3a5118e5beed80e563d14ff9ab1ccc7
fariv <farivind@gmail.com>
2022-06-17 11:55:59 +0700

    modified io connection

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 const sslServer = https.createServer({
-    key:fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')) , 
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-},app)
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app)
 
 global.io = new Server(sslServer, {
   cors: {
@@ -25,14 +25,40 @@ global.io = new Server(sslServer, {
   }
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    
-    socket.on('disconnect', () => {
-      console.log(`socket disconnected`);
-    })
-  });
+let onlineUsers = []
 
+const addNewUser = (username, socketId) => {
+  console.log(username, socketId, 'adnew');
+  !onlineUsers.some((user) => user.username === username) &&
+    onlineUsers.push({ username, socketId });
+}
+
+
+const removeUser = (socketId) => {
+  console.log(socketId, 'remove');
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId)
+}
+
+
+const getUser = (username)=> {
+  return onlineUsers.find((user)=> user.username === username)
+}
+
+io.on('connection', (socket) => {
+  
+  socket.on("newUser", (data) => {
+    console.log(data.user, data.socketId, 'username');
+    addNewUser(data.user, data.socketId)
+  })
+
+  console.log(onlineUsers, 'online');
+ 
+  socket.on('disconnect', () => {
+    console.log(socket.newUser);
+    removeUser(socket.id)
+    console.log('disconnect');
+  })
+});
 
 
 
@@ -45,6 +71,6 @@ app.use(cors());
 app.use('/api-gateway/', indexRouter);
 
 
-sslServer.listen(3443, ()=> console.log('secure server run on port 3443'))
+sslServer.listen(3443, () => console.log('secure server run on port 3443'))
 
 module.exports = app;
