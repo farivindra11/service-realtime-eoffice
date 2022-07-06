0bf42600c97cde53378845a79ab5500ef01a6529
fariv <farivind@gmail.com>
2022-07-06 11:31:23 +0700

    change api adapter done

--- _helpers/apiAdapter.js
+++ _helpers/apiAdapter.js
@@ -3,7 +3,7 @@ const https = require('https')
 const fs = require('fs')
 
 const httpsAgent = new https.Agent({
-    rejectUnauthorized: false, // (NOTE: this will disable client verification)
+    rejectUnauthorized: false, //================ (NOTE: this will disable client verification) =====================
     cert: fs.readFileSync("./cert/cert.pem"),
     key: fs.readFileSync("./cert/key.pem")
   })
@@ -12,6 +12,6 @@ const httpsAgent = new https.Agent({
 module.exports = (baseUrl) => {
     return axios.create({
         baseURL: baseUrl,
-        httpsAgent, //SSL certification    
+        httpsAgent, //================= SSL certification =============    
     })
 }
