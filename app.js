07438042267966bb38a56c76fdcbb5d3610c5f6f
fariv <farivind@gmail.com>
2022-07-06 11:32:29 +0700

    realtime notification done

--- app.js
+++ app.js
@@ -2,64 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const cors = require("cors");
 const path = require('path');
 const https = require('https')
 const fs = require('fs')
 const { Server } = require("socket.io");
-const cors = require("cors");
+const apiAdapter = require('./_helpers/apiAdapter')
 
 
+const { API_GATEWAY_EOFFICE } = process.env;
+const api = apiAdapter(API_GATEWAY_EOFFICE);
 
 const indexRouter = require('./routes/index');
-
 const app = express();
 
 const sslServer = https.createServer({
+  rejectUnauthorized: false,
   key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
   cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
 }, app)
-
-global.io = new Server(sslServer, {
-  cors: {
-    origin: 'http://localhost:3000'
-  }
-});
-
-let onlineUsers = []
-
-const addNewUser = (username, socketId) => {
-  console.log(username, socketId, 'adnew');
-  !onlineUsers.some((user) => user.username === username) &&
-    onlineUsers.push({ username, socketId });
-}
-
-
-const removeUser = (socketId) => {
-  console.log(socketId, 'remove');
-  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId)
-}
-
-
-const getUser = (username)=> {
-  return onlineUsers.find((user)=> user.username === username)
-}
-
-io.on('connection', (socket) => {
-  
-  socket.on("newUser", (data) => {
-    console.log(data.user, data.socketId, 'username');
-    addNewUser(data.user, data.socketId)
-  })
-
-  console.log(onlineUsers, 'online');
- 
-  socket.on('disconnect', () => {
-    console.log(socket.newUser);
-    removeUser(socket.id)
-    console.log('disconnect');
-  })
-});
-
 
 
 app.use(logger('dev'));
@@ -68,9 +29,98 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
-app.use('/api-gateway/', indexRouter);
+app.use('/api-gateway', indexRouter);
+
+global.io = new Server(sslServer, {     //=============== proxy ====================
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+});
 
 
-sslServer.listen(3443, () => console.log('secure server run on port 3443'))
+const addNewUser = async (data, socketId) => {    //===================== add new user online ==============
+  try {
+    const userData = await api.get('/user-online')
+    const dataUser = userData.data.data
+
+    !dataUser.some((user) => user.username === data.user) && await api.post('/user-online', {
+      username: data.user,
+      kode_opd: data.socketId,
+      socketId: socketId
+    })
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const removeUser = async (socketId) => {      //========================= remove user online ===========
+  try {
+    const userData = await api.get('/user-online')
+    const dataUser = userData.data.data
+    dataUser.forEach(async (element) => {
+
+      if (element.socketId === socketId) {
+        const id = element.id
+        await api.delete(`/user-online/${id}`)
+      }
+
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
+const getUser = async (opdPenerima) => {     //============================== GET user online ====================
+  const data = await api.get('/user-online')
+  const resp = data.data.data
+
+  return resp.find((user) => user.kode_opd === opdPenerima)
+}
+
+
+io.on('connection', (socket) => {     //================ connection socket.io ===============
+  console.log('user connected')
+
+  socket.on("newUser", (data) => {    //================ add user connected ================
+    addNewUser(data, socket.id)
+  })
+
+  socket.on("sendNotif", ({ sender, opdPenerima, pesan }) => {    //============== take event from socket client ===============
+    const receiver = getUser(opdPenerima);
+
+    receiver.then(async (res) => {
+      if (res) {
+        await api.post('/user-notif', {
+          sender: sender,
+          pesan: pesan,
+          opd_penerima: opdPenerima
+        })
+        const data = await api.get(`/user-notif?kodeopd=${opdPenerima}`)
+        io.to(res.socketId).emit('getNotif', data.data.data)    //======================= send event notif to socket client ============
+      } else {
+        await api.post('/user-notif', {
+          sender: sender,
+          pesan: pesan,
+          opd_penerima: opdPenerima
+        })
+      }
+    })
+
+  });
+
+  socket.on('disconnect', () => {     //====================== user on disconnect ===================
+    removeUser(socket.id)
+    console.log('users on disconnect');
+  })
+
+});
+
+
+
+sslServer.listen(3001, () => console.log('secure server run on port 3001'))
+sslServer.timeout = 10000, console.log('server timeout 10 Sec.');
 
 module.exports = app;
