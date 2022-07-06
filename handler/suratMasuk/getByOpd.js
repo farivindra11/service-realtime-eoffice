268ed338dcc2a458ec8aba568c100ad354d6912a
fariv <farivind@gmail.com>
2022-07-06 11:30:34 +0700

    api gateway realtime for surat masuk done

--- handler/suratMasuk/getByOpd.js
+++ handler/suratMasuk/getByOpd.js
@@ -3,24 +3,30 @@ const { API_GATEWAY_EOFFICE } = process.env;
 
 const api = apiAdapter(API_GATEWAY_EOFFICE);
 
-module.exports = async (req, res) => {
+const suratMasuk = async (req, res) => {
 
     try {
-        console.log('masuk api sini from frontend');
         const kodeopd = req.query.kodeopd
         const token = req.headers.authorization
 
-        const data = await api.get(`/surat-masuk?kodeopd=${kodeopd}`,
-            {
-                headers: {
-                    'Authorization': token || ''
-                }
-            });
-        io.emit('firstEven', data.data)
-        res.status(200);
-
-
+        const data = await api.get(`/surat-masuk?kodeopd=${kodeopd}`, {
+            headers: {
+                Authorization: token || ''
+            }
+        });
+        if (data.data.status === false) {
+            res.status(data.data.status_code).json(data.data)
+        } else {
+            io.emit('suratMasuk', data.data)
+            res.status(200).json(data.data)
+        }
     } catch (error) {
-        console.log(error);
+        res.status(500).json({
+            message: error,
+        });
     }
+}
+
+module.exports = {
+    suratMasuk
 }
